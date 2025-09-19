d1c32b35315f2a512a12daa7b1847b5deff60b04
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-09-19 20:35:52 +0545

    added email producer for forgot password

---
+++ src/producers/email-producer.js
@@ -0,0 +1,11 @@
+const { mailerQueue } = require("../queues/mailer-queue");
+
+const SEND_FORGOT_PASSWORD_EMAIL_PAYLOAD = "send-forgot-password-email";
+
+const addForgotPasswordEmailJob = async (payload) => {
+  await mailerQueue.add(SEND_FORGOT_PASSWORD_EMAIL_PAYLOAD, { payload });
+};
+
+module.exports = {
+  addForgotPasswordEmailJob,
+};
